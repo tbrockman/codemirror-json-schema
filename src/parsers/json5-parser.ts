f05f8856846bc474d506d732aba00cd952c54745
theodore brockman <tmanbrock@gmail.com>
2024-09-24 22:52:44 -0400

    Comment out for our build.

--- src/parsers/json5-parser.ts
+++ src/parsers/json5-parser.ts
@@ -2,29 +2,29 @@
  * Mimics the behavior of `json-source-map`'s `parseJSONDocument` function using codemirror EditorState... for json5
  */
 
-import { json5 as json5mode } from "codemirror-json5";
-import json5 from "json5";
+// import { json5 as json5mode } from "codemirror-json5";
+// import json5 from "json5";
 import { EditorState } from "@codemirror/state";
-import { parse } from "best-effort-json-parser";
-import { getJsonPointers } from "../utils/json-pointers";
-import { MODES } from "../constants";
+// import { parse } from "best-effort-json-parser";
+// import { getJsonPointers } from "../utils/json-pointers";
+// import { MODES } from "../constants";
 
 /**
  * Return parsed data and json5 pointers for a given codemirror EditorState
  * @group Utilities
  */
 export function parseJSON5DocumentState(state: EditorState) {
-  let data = null;
-  try {
-    data = json5.parse(state.doc.toString());
-    // return pointers regardless of whether JSON.parse succeeds
-  } catch {
-    try {
-      data = parse(state.doc.toString());
-    } catch {}
-  }
-  const pointers = getJsonPointers(state, MODES.JSON5);
-  return { data, pointers };
+  // let data = null;
+  // try {
+  //   data = json5.parse(state.doc.toString());
+  //   // return pointers regardless of whether JSON.parse succeeds
+  // } catch {
+  //   try {
+  //     data = parse(state.doc.toString());
+  //   } catch {}
+  // }
+  // const pointers = getJsonPointers(state, MODES.JSON5);
+  // return { data, pointers };
 }
 
 /**
--- src/parsers/yaml-parser.ts
+++ src/parsers/yaml-parser.ts
@@ -2,22 +2,22 @@
  * Mimics the behavior of `json-source-map`'s `parseJSONDocument` function using codemirror EditorState... for YAML
  */
 
-import { yaml } from "@codemirror/lang-yaml";
-import YAML from "yaml";
+// import { yaml } from "@codemirror/lang-yaml";
+// import YAML from "yaml";
 import { EditorState } from "@codemirror/state";
-import { getJsonPointers } from "../utils/json-pointers";
-import { MODES } from "../constants";
+// import { getJsonPointers } from "../utils/json-pointers";
+// import { MODES } from "../constants";
 
 /**
  * Return parsed data and YAML pointers for a given codemirror EditorState
  * @group Utilities
  */
 export function parseYAMLDocumentState(state: EditorState) {
-  let data = null;
-  try {
-    data = YAML.parse(state.doc.toString());
-    // return pointers regardless of whether YAML.parse succeeds
-  } catch {}
-  const pointers = getJsonPointers(state, MODES.YAML);
-  return { data, pointers };
+  // let data = null;
+  // try {
+  //   data = YAML.parse(state.doc.toString());
+  //   // return pointers regardless of whether YAML.parse succeeds
+  // } catch {}
+  // const pointers = getJsonPointers(state, MODES.YAML);
+  // return { data, pointers };
 }
